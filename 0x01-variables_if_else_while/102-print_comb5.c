12ed28d88efd22af1fed9672e30e6d281bd166f4
AdeSky <fayemiwoade@gmail.com>
2022-10-14 19:28:28 -0700

    Correcting task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (a = 0; a <= 98; a++)
 	{
-		for (b = 0; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			putchar((a % 10) + '0');
+			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((b % 10) + '0');
+			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
 			if (a == 98 && b == 99)
