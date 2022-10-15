d3ca3fde0bf5f208284a0ffcdf4e568c7d37cb46
AdeSky <fayemiwoade@gmail.com>
2022-10-14 17:20:46 -0700

    Adding task 5 file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,15 @@
 /**
  * main -Prints all sngle digit numbers of base 10 starting from 0
  *
- * Result: Always 0 (Success);
+ * Return: Always 0 (Success);
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 10; n++)
-		printf("%d\n", n);
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+
 	return (0);
 }
