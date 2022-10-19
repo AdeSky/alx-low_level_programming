3fd6cf61df2ab868b4efc156445b4ca6076c87d7
AdeSky <fayemiwoade@gmail.com>
2022-10-19 05:19:08 -0700

    Adding 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,15 +11,14 @@ int main(void)
 
 	printf("Infinte loo incoming :(\n");
 
-	/**
 	i = 0;
 
-	
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	*/
+
+	 /* while (i < 10)*/
+	 /* {*/
+	 /* putchar(i);*/
+	 /* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
